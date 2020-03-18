8cac124c50d0ff554989f48e004bbd6ee33cfe2b
Chami Rachid <chamirachid1@gmail.com>
2020-03-18 08:00:37 -0700

    Added eth_getBalance method

--- src/main/kotlin/com/epirus/local/Server.kt
+++ src/main/kotlin/com/epirus/local/Server.kt
@@ -9,8 +9,15 @@ import io.ktor.routing.post
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import org.web3j.abi.datatypes.Address
+import org.web3j.crypto.Hash
+import org.web3j.evm.Configuration
+import org.web3j.evm.EmbeddedEthereum
+import org.web3j.evm.PassthroughTracer
 
 class Server{
+
+    val embeddedEthereum: EmbeddedEthereum = EmbeddedEthereum(Configuration(Address("0xc94770007dda54cF92009BFF0dE90c06F603a09f"), 10), PassthroughTracer())
 
     fun start() {
         val server = embeddedServer(Netty, 8080) {
@@ -18,6 +25,7 @@ class Server{
                 post("/") {
                     try {
                         val req : String = call.receive<String>()
+
                         val request : Request = Klaxon().parse<Request>(req) ?: throw Exception("Incorrect request: \n $req")
                         val json_output = Klaxon().toJsonString(Response(request.id, request.jsonrpc, makeCall(request)))
 
@@ -32,8 +40,11 @@ class Server{
         server.start(true)
     }
 
-    fun makeCall(lRequest: Request): String {
-        return when (lRequest.method) {
+    fun makeCall(request: Request): String {
+        return when (request.method) {
+            "eth_blockNumber" -> embeddedEthereum.ethBlockNumber()
+            "eth_getTransactionCount" -> embeddedEthereum.getTransactionCount(Address(request.params[0]), request.params[1]).toString()
+            "eth_getBalance" -> embeddedEthereum.ethGetBalance(Address(request.params[0]), request.params[1]) ?: "0"
             else -> return "Not supported yet!"
         }
     }
