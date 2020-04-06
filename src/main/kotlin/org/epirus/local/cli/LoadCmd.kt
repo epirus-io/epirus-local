93fd8d08f08cb9aafdcc8ac39142ce2d524696f4
Chami Rachid <chamirachid1@gmail.com>
2020-04-06 00:44:44 -0700

    Revert "Fixed LedgerConfig creation"
    This reverts commit afc228aaca4ba6c088964cba68f54fea191bda34.

--- src/main/kotlin/org/epirus/local/cli/LoadCmd.kt
+++ src/main/kotlin/org/epirus/local/cli/LoadCmd.kt
@@ -51,7 +51,10 @@ class LoadCmd : Callable<Int> {
 
     @KtorExperimentalAPI
     override fun call(): Int {
-        val ledgerConfiguration = LedgerConfiguration(genesis = genesisFilePath)
+        val ledgerConfiguration = LedgerConfiguration(
+                genesis = genesisFilePath,
+                accounts = null
+        )
         val env = applicationEngineEnvironment {
             connector {
                 host = cliHost
